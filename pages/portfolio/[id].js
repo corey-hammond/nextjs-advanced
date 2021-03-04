572f2dfc783e5509e90de8a8580e3e1820f6c08e
Corey Hammond <coreyjhammond@gmail.com>
2021-03-04 11:30:19 -0800

    initial commit

---
+++ pages/portfolio/[id].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  );
+};
+
+export default PortfolioProjectPage;
